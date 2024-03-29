468426383bba142904eaa72170fc34c42f9c44f1
Alexander Ratajczak <a4blue@hotmail.de>
2024-03-29 10:00:01 +0100

    homepage working for now

--- nixos/homepage-container.nix
+++ nixos/homepage-container.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
-
+let THIS_DIR = builtins.toString ./configs/homepage; in
 {
-  let THIS_DIR = builtins.toString ./configs/homepage; in
+#  THIS_DIR = builtins.toString ./configs/homepage;
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers = {
     homepage-container = {
       image = "ghcr.io/gethomepage/homepage:latest";
       autoStart = true;
       ports = [ "3000:3000" ];
-      volumes = [ "${THIS_DIR}:/app/config" "/var/run/docker.sock:/var/run/docker.sock" ];
+      volumes = [ "/home/a4blue/nix-config/nixos/configs/homepage:/app/config" "/var/run/docker.sock:/var/run/docker.sock" ];
     };
   };
   networking.firewall.allowedTCPPorts = [ 3000 ];

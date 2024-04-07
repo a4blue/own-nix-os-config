c935792ad9d35ab3b24a964e9458a8e70ac92eae
Alexander Ratajczak <a4blue@hotmail.de>
2024-04-07 03:24:06 +0200

    fix

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -13,7 +13,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     #inputs.sops-nix.homeManagerModules.
-    inputs.impermanence.homeManagerModules.impermanence
+    inputs.impermanence.nixosModules.home-manager.impermanence
   ];
 
   home = {

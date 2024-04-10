d040f8d327844d4278790019e721d0efdecdad98
Alexander Ratajczak <a4blue@hotmail.de>
2024-04-11 00:16:35 +0200

    need a clean boot

--- modules/nixos/re-create-root.nix
+++ modules/nixos/re-create-root.nix
@@ -5,7 +5,7 @@
   utils,
   ...
 }: {
-  boot.initrd.systemd.packages = with pkgs; [gnugrep findutils];
+  boot.initrd.systemd.packages = with pkgs; [gnugrep findutils keyutils];
   boot.initrd.systemd.services = {
     "recreate-root" = {
       description = "";
@@ -28,7 +28,8 @@
         RemainAfterExit = true;
       };
       script = ''
-        ${config.boot.initrd.systemd.package}/bin/systemd-ask-password --timeout=0 "enter passphrase for / (poor man's impermanence)" | exec ${pkgs.bcachefs-tools}/bin/bcachefs unlock "/dev/nvme0n1p3"
+        keyctl link @u @s
+        ${config.boot.initrd.systemd.package}/bin/systemd-ask-password --timeout=0 "enter passphrase for / (poor man's impermanence)" | exec ${pkgs.bcachefs-tools}/bin/bcachefs unlock -k session "/dev/nvme0n1p3"
         mkdir /bcachefs_recreate_root
         mount /dev/nvme0n1p3 /bcachefs_recreate_root
         find /bcachefs_recreate_root -maxdepth 1 | grep -v "/bcachefs_recreate_root/nix\|/bcachefs_recreate_root/persistent\|^/bcachefs_recreate_root$" | xargs rm -rf

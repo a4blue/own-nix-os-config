96d1b90bfaee68171949111f21b89dc7f61c5375
Alexander Ratajczak <a4blue@hotmail.de>
2024-04-13 11:35:39 +0200

    pls fix photoprism

--- modules/nixos/photoprism.nix
+++ modules/nixos/photoprism.nix
@@ -35,10 +35,11 @@
     proxyPass = "http://localhost:2342";
     proxyWebsockets = true;
     extraConfig = ''
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header Host $host;
       proxy_buffering off;
       client_max_body_size 500m;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_redirect off;
     '';
   };
 

8e2ad706f9d27b075064098790b7b1a61fa7848a
Alexander Ratajczak <a4blue@hotmail.de>
2024-04-20 16:21:47 +0200

    fixed port for forgejo

--- modules/nixos/forgejo.nix
+++ modules/nixos/forgejo.nix
@@ -7,14 +7,14 @@
       server = {
         DOMAIN = "homelab.armadillo-snake.ts.net";
         ROOT_URL = "https://${config.services.forgejo.settings.server.DOMAIN}/forgejo/";
-        HTTP_PORT = 78945;
+        HTTP_PORT = 3001;
       };
       #service.DISABLE_REGISTRATION = true;
     };
   };
   services.nginx.virtualHosts."homelab.armadillo-snake.ts.net".locations."/forgejo" = {
     recommendedProxySettings = true;
-    proxyPass = "http://localhost:78945";
+    proxyPass = "http://localhost:3001";
     extraConfig = ''
       client_max_body_size 512M;
     '';

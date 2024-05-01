342a59122add722af6f654abf708ea91423c50dc
Alexander Ratajczak <a4blue@hotmail.de>
2024-05-01 19:17:07 +0200

    nix ld

--- systems/wsl2/configuration.nix
+++ systems/wsl2/configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     inputs.sops-nix.nixosModules.sops
     inputs.home-manager.nixosModules.home-manager
+    inputs.nix-ld.nixosModules.nix-ld
     ../../modules/nixos/base.nix
     ../../modules/nixos/home-manager-base.nix
   ];
@@ -18,7 +19,21 @@
         ./../../modules/home-manager/base.nix
       ];
     };
+    nixos = {
+      home = {
+        username = "nixos";
+        homeDirectory = "/home/nixos";
+        stateVersion = "24.05";
+        keyboard = null;
+      };
+    };
   };
+
+  programs.nix-ld.dev.enable = true;
+
+  programs.nix-ld.dev.libraries = with pkgs; [
+    pkgs.stdenv.cc.cc
+  ];
 
   wsl.enable = true;
   wsl.defaultUser = "nixos";

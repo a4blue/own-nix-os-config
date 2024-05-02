2801cc044bd9444de7e82898a3ed264a77deb650
Alexander Ratajczak <a4blue@hotmail.de>
2024-05-02 23:37:48 +0200

    fix some neovim deps

--- modules/nixos/neovim/default.nix
+++ modules/nixos/neovim/default.nix
@@ -15,7 +15,46 @@
     name = "lazyvim-nix-treesitter-parsers";
     paths = pkgs.vimPlugins.nvim-treesitter.withAllGrammars.dependencies;
   };
+
+  # codelldb executable is not exported by default
+  codelldb = pkgs.writeShellScriptBin "codelldb" ''
+    nix shell --impure --expr 'with import (builtins.getFlake "nixpkgs") {}; writeShellScriptBin "codelldb" "''${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb $@"' --command codelldb "$@"
+  '';
+
+  # cmake-lint is used as cmakelint
+  cmakelint = pkgs.writeShellScriptBin "cmakelint" ''
+    nix shell nixpkgs#cmake-format --command cmake-lint "$@"
+  '';
+
+  clangd = pkgs.writeShellScriptBin "clangd" ''
+    if [ -f /opt/vector-clang-tidy/bin/clangd ]; then
+      /opt/vector-clang-tidy/bin/clangd "$@"
+    else
+      nix shell nixpkgs#clang-tools_16 --command clangd "$@"
+    fi
+  '';
+
+  make-lazy = pkg: bin:
+    pkgs.writeShellScriptBin "${bin}" ''
+      nix shell nixpkgs#${pkg} --command ${bin} "$@"
+    '';
 in {
+  environment.systemPackages = with pkgs; [
+    fd
+    lazygit
+    ripgrep
+    fish
+    shfmt
+    stylua
+    xdg-utils
+    luajitPackages.jsregexp
+    markdownlint-cli
+    cmakelint
+    clangd
+    nil
+    tree-sitter
+    nodejs_20
+  ];
   programs.neovim = {
     enable = true;
 

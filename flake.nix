570e910baabeead1f6669295bc1d5e971c15596f
Alexander Ratajczak <a4blue@hotmail.de>
2024-09-12 18:38:05 +0200

    update + new smbconf

--- flake.nix
+++ flake.nix
@@ -26,27 +26,6 @@
       url = "github:bluskript/nix-inspect";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    #flake-parts.url = "github:hercules-ci/flake-parts";
-    #flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-
-    # Plugins not available in nixpkgs
-    huez-nvim = {
-      url = "github:vague2k/huez.nvim";
-      flake = false;
-    };
-    blame-me-nvim = {
-      url = "github:hougesen/blame-me.nvim";
-      flake = false;
-    };
-    cmake-tools-nvim = {
-      url = "github:Civitasv/cmake-tools.nvim";
-      flake = false;
-    };
-    cmake-gtest-nvim = {
-      url = "github:hfn92/cmake-gtest.nvim";
-      flake = false;
-    };
   };
 
   outputs = {
--- modules/nixos/samba.nix
+++ modules/nixos/samba.nix
@@ -1,22 +1,18 @@
 {config, ...}: {
   services.samba = {
     enable = true;
-    securityType = "user";
     openFirewall = true;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = smbnix
-      netbios name = smbnix
-      security = user
-      #use sendfile = yes
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.178.0/24 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
-    shares = {
+    settings = {
+      global = {
+        workgroup = "WORKGROUP";
+        "server string" = "smbnix";
+        "netbios name" = "smbnix";
+        security = "user";
+        "hosts allow" = "192.168.178.0/24 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
       private = {
         path = "/SDMedia/smb";
         browseable = "yes";

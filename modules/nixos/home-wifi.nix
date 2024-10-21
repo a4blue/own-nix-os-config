5bef9f098a1babc138f319735cb91ed5ffae8eb2
Alexander Ratajczak <a4blue@hotmail.de>
2024-10-21 21:33:09 +0200

    wrong ssid ...

--- modules/nixos/home-wifi.nix
+++ modules/nixos/home-wifi.nix
@@ -7,7 +7,10 @@
   config.sops.secrets.home_wifi_psk = {};
   config.networking.wireless = lib.mkIf config.networking.wireless.enable {
     secretsFile = config.sops.secrets.home_wifi_psk.path;
-    networks."NoNameWLAN" = {
+    networks."NoNameWLAN 5Ghz" = {
+      pskRaw = "ext:HOME_WIFI_PSK";
+    };
+    networks."NoNameWLAN 2.4Ghz" = {
       pskRaw = "ext:HOME_WIFI_PSK";
     };
   };
@@ -15,9 +18,9 @@
   config.networking.networkmanager = lib.mkIf config.networking.networkmanager.enable {
     ensureProfiles.environmentFiles = [config.sops.secrets.home_wifi_psk.path];
     ensureProfiles.profiles = {
-      "NoNameWLAN" = {
+      "NoNameWLAN 5Ghz" = {
         connection = {
-          id = "NoNameWLAN";
+          id = "NoNameWLAN 5Ghz";
           permissions = "";
           type = "wifi";
         };
@@ -33,7 +36,33 @@
         wifi = {
           mac-address-blacklist = "";
           mode = "infrastructure";
-          ssid = "NoNameWLAN";
+          ssid = "NoNameWLAN 5Ghz";
+        };
+        wifi-security = {
+          auth-alg = "open";
+          key-mgmt = "wpa-psk";
+          psk = "$HOME_WIFI_PSK";
+        };
+      };
+      "NoNameWLAN 2.4Ghz" = {
+        connection = {
+          id = "NoNameWLAN 2.4Ghz";
+          permissions = "";
+          type = "wifi";
+        };
+        ipv4 = {
+          dns-search = "";
+          method = "auto";
+        };
+        ipv6 = {
+          addr-gen-mode = "stable-privacy";
+          dns-search = "";
+          method = "auto";
+        };
+        wifi = {
+          mac-address-blacklist = "";
+          mode = "infrastructure";
+          ssid = "NoNameWLAN 2.4Ghz";
         };
         wifi-security = {
           auth-alg = "open";

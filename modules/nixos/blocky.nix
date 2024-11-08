6ba300c5904ea8df534bec9cc842901f0479f96f
Alexander Ratajczak <a4blue@hotmail.de>
2024-11-08 21:38:26 +0100

    new dns server

--- modules/nixos/blocky.nix
+++ modules/nixos/blocky.nix
@@ -4,13 +4,20 @@
     settings = {
       ports.dns = 53;
       caching.prefetching = true;
-      caching.prefetchExpires = "12h";
+      caching.prefetchExpires = "2h";
       caching.cacheTimeNegative = "1m";
+      caching.minTime = "10m";
+
+      queryLog.type = "csv";
+      queryLog.target = "/var/lib/blocky";
+      queryLog.logRetentionDays = 7;
+
       log.level = "warn";
       customDNS = {
         customTTL = "1h";
         filterUnmappedTypes = true;
         mapping = {
+          "fritz.box" = "192.168.178.1";
           "homelab.a4blue.me" = "192.168.178.64";
           "*.homelab.a4blue.me" = "192.168.178.64";
           "*.homelab.internal" = "192.168.178.64";
@@ -20,18 +27,48 @@
         };
       };
       upstreams.groups.default = [
-        # Digital Courage
+        # https://digitalcourage.de/support/zensurfreier-dns-server
+        # UnCensored ✅
         "tcp-tls:dns3.digitalcourage.de:853"
-        # Digitale Gesellschaft
+        # https://www.digitale-gesellschaft.ch/dns/
+        # UnCensored ✅
         "https://dns.digitale-gesellschaft.ch/dns-query"
-        # Uncensored DNS Anycast
+        # https://blog.uncensoreddns.org/dns-servers/
+        # UnCensored ✅
         "https://anycast.uncensoreddns.org/dns-query"
-        # Uncensored DNS Unicast
+        # https://blog.uncensoreddns.org/dns-servers/
+        # UnCensored ✅
         "https://unicast.uncensoreddns.org/dns-query"
-        # DNS Forge
+        # https://dnsforge.de/
+        # Censored 🚫
         "https://dnsforge.de/dns-query"
-        # Quad9 (Should i trust them ?)
+        # https://quad9.net/de/service/service-addresses-and-features
+        # Censored 🚫
         "https://dns.quad9.net/dns-query"
+        # https://mullvad.net/de/help/dns-over-https-and-dns-over-tls
+        # UnCensored ✅
+        "https://dns.mullvad.net/dns-query"
+        # https://blahdns.com/
+        # Censored 🚫
+        "https://doh-de.blahdns.com/dns-query"
+        # https://dismail.de/info.html#dns
+        # Censored 🚫
+        "tcp-tls:fdns2.dismail.de:853"
+        # https://dns.njal.la/
+        # UnCensored ✅
+        "https://dns.njal.la/dns-query"
+        # https://opennameserver.org/
+        # UnCensored ✅
+        "https://ns.opennameserver.org/dns-query"
+        # https://controld.com/free-dns
+        # UnCensored ✅
+        "https://freedns.controld.com/p0"
+        # https://servers.opennic.org/edit.php?srv=ns2.de.dns.opennic.glue
+        # UnCensored ✅
+        "https://doh.kekew.info/dns-query"
+        # https://servers.opennic.org/edit.php?srv=ns23.de.dns.opennic.glue
+        # UnCensored ✅
+        "https://dns3.opennic.probst.click/"
       ];
       bootstrapDns = [
         {
@@ -54,10 +91,17 @@
           upstream = "https://unicast.uncensoreddns.org/dns-query";
           ips = ["89.233.43.71" "2a01:3a0:53:53::"];
         }
+        {
+          upstream = "https://dns.mullvad.net/dns-query";
+          ips = ["194.242.2.2" "2a07:e340::2"];
+        }
       ];
       blocking = {
-        blackLists = {
-          ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
+        denylists = {
+          ads = [
+            "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+            "https://perflyst.github.io/PiHoleBlocklist/SmartTV.txt"
+          ];
         };
         clientGroupsBlock = {
           default = ["ads"];
@@ -67,4 +111,13 @@
   };
   networking.firewall.allowedTCPPorts = [53];
   networking.firewall.allowedUDPPorts = [53];
+
+  environment.persistence."/persistent" = {
+    directories = [
+      {
+        directory = "/var/lib/blocky";
+        mode = "0740";
+      }
+    ];
+  };
 }

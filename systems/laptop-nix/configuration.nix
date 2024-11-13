c664c84883cfb7e67f945873a779f9f8c44ce842
Alexander Ratajczak <a4blue@hotmail.de>
2024-11-13 23:42:22 +0100

    firefox config, new nixvim version

--- systems/laptop-nix/configuration.nix
+++ systems/laptop-nix/configuration.nix
@@ -121,6 +121,23 @@
       home.packages = with pkgs; [
         proton-pass
       ];
+      programs.firefox = {
+        enable = true;
+        policies = {
+          DisableTelemetry = true;
+          DisableFirefoxStudies = true;
+          EnableTrackingProtection = {
+            Value = true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+          DisableAccounts = true;
+          DisableFirefoxAccounts = true;
+          DisablePocket = true;
+          DontCheckDefaultBrowser = true;
+        };
+      };
     };
   };
 

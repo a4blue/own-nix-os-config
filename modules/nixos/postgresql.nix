d89267b0282db249ef12aa02a89896169c737208
Alexander Ratajczak <a4blue@hotmail.de>
2025-06-22 18:22:15 +0200

    added postgres migration

--- modules/nixos/postgresql.nix
+++ modules/nixos/postgresql.nix
@@ -1,29 +1,42 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
   services.postgresql = {
     enable = true;
-    #identMap = ''
-    ## ArbitraryMapName systemUser DBUser
-    #   superuser_map      root      postgres
-    #   superuser_map      postgres  postgres
-    #   # Let other names login as themselves
-    #   superuser_map      /^(.*)$   \1
-    #'';
-    #initialScript = pkgs.writeText "init-sql-script" ''
-    #  ALTER USER paperless PASSWORD 'paperless';
-    #'';
+    package = pkgs.postgresql_16;
   };
 
-  # temp debug
-  #services.pgadmin = {
-  #  enable = true;
-  #  openFirewall = true;
-  #  initialPasswordFile = "/nix/secret/weak_password";
-  #  initialEmail = "test@test.com";
-  #};
+  environment.systemPackages = [
+    (let
+      newPostgres =
+        pkgs.postgresql_17.withPackages (pp: [
+        ]);
+      cfg = config.services.postgresql;
+    in
+      pkgs.writeScriptBin "upgrade-pg-cluster" ''
+        set -eux
+        # XXX it's perhaps advisable to stop all services that depend on postgresql
+        systemctl stop postgresql
+
+        export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
+        export NEWBIN="${newPostgres}/bin"
+
+        export OLDDATA="${cfg.dataDir}"
+        export OLDBIN="${cfg.finalPackage}/bin"
+
+        install -d -m 0700 -o postgres -g postgres "$NEWDATA"
+        cd "$NEWDATA"
+        sudo -u postgres "$NEWBIN/initdb" -D "$NEWDATA" ${lib.escapeShellArgs cfg.initdbArgs}
+
+        sudo -u postgres "$NEWBIN/pg_upgrade" \
+          --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
+          --old-bindir "$OLDBIN" --new-bindir "$NEWBIN" \
+          "$@"
+      '')
+  ];
 
   environment.persistence."${config.modules.impermanenceExtra.defaultPath}" = {
     directories = [

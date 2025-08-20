6853e94017c07bc7938cbe4f92cce8ab55baf9eb
Alexander Ratajczak <a4blue@hotmail.de>
2025-08-20 21:53:56 +0200

    update and reset pipewire config, inputs are crackling apparently

--- systems/desktop/configuration.nix
+++ systems/desktop/configuration.nix
@@ -145,25 +145,25 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      extraConfig.pipewire-pulse."92-low-latency" = {
-        "context.properties" = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = {};
-          }
-        ];
-        "pulse.properties" = {
-          "pulse.min.req" = "32/48000";
-          "pulse.min.quantum" = "32/48000";
-          "pulse.default.req" = "960/48000";
-          "pulse.max.req" = "1024/48000";
-          "pulse.max.quantum" = "1024/48000";
-        };
-        "stream.properties" = {
-          "node.latency" = "32/48000";
-          "resample.quality" = 1;
-        };
-      };
+      #extraConfig.pipewire-pulse."92-low-latency" = {
+      #  "context.properties" = [
+      #    {
+      #      name = "libpipewire-module-protocol-pulse";
+      #      args = {};
+      #    }
+      #  ];
+      #  "pulse.properties" = {
+      #    "pulse.min.req" = "32/48000";
+      #    "pulse.min.quantum" = "32/48000";
+      #    "pulse.default.req" = "960/48000";
+      #    "pulse.max.req" = "1024/48000";
+      #    "pulse.max.quantum" = "1024/48000";
+      #  };
+      #  "stream.properties" = {
+      #    "node.latency" = "32/48000";
+      #    "resample.quality" = 1;
+      #  };
+      #};
       extraConfig.pipewire."51-disable-suspension" = {
         "monitor.alsa.rules" = [
           {

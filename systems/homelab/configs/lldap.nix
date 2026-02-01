02da04432e19196725d13fe1cb897408c29aee9d
Alexander Ratajczak <a4blue@hotmail.de>
2026-02-01 09:43:30 +0100

    maybe fixed secrets ?

--- systems/homelab/configs/lldap.nix
+++ systems/homelab/configs/lldap.nix
@@ -1,5 +1,9 @@
 {config, ...}: {
-  sops.secrets.lldap_ldapUserPassword = {
+  sops.secrets."lldap/userPassword" = {
+    owner = "lldap";
+    group = "lldap";
+  };
+  sops.secrets."lldap/jwtSecret" = {
     owner = "lldap";
     group = "lldap";
   };
@@ -14,9 +18,9 @@
       ldap_port = 3890;
       ldap_base_dn = "dc=home.a4blue,dc=me";
 
-      ldap_user_pass_file = config.sops.secrets.lldap_ldapUserPassword.path;
+      ldap_user_pass_file = config.sops.secrets."lldap/userPassword".path;
       force_ldap_user_pass_reset = "always";
-      jwt_secret_file = "d34ffac7fa9af00f017a4f2e298aa4df766c2c8fa88bcfdc89bea676c8bfe56646b3fe0d9ae35fb5a367ffaf641f07500c50f1d81d5aab4106599dd35e6a5049";
+      jwt_secret_file = config.sops.secrets."lldap/jwtSecret".path;
     };
   };
   users.users.lldap = {

3ff19226f26d08fdf6c86c717b578f26f2c1fe18
Alexander Ratajczak <a4blue@hotmail.de>
2026-02-02 16:26:51 +0100

    more fixes for lldap

--- systems/homelab/configs/lldap.nix
+++ systems/homelab/configs/lldap.nix
@@ -26,23 +26,14 @@ in {
       ldap_user_pass_file = config.sops.secrets."lldap/userPassword".path;
       force_ldap_user_pass_reset = "always";
       jwt_secret_file = config.sops.secrets."lldap/jwtSecret".path;
+      # I dont know if this should be secret or not
+      # See https://github.com/lldap/lldap/blob/main/lldap_config.docker_template.toml#L108
+      # It claims that even with that knowledge its pretty hard to crack
+      key_seed = "b7e7e121e2f549b63362d5fd104a6f0fb46ba40ed98e20f1d3e6831889d30f421f367dedfb656333b22724d8187860eecb7fea578e98d4c1e315c09c1b62147f";
     };
     database = {
       type = "postgresql";
     };
-    environment = {
-      LLDAP_KEY_FILE = "/var/lib/lldap-data/server_key";
-    };
-  };
-  environment.persistence."${config.modules.impermanenceExtra.defaultPath}" = {
-    directories = [
-      {
-        directory = "/var/lib/lldap-data";
-        mode = "740";
-        user = "lldap";
-        group = "lldap";
-      }
-    ];
   };
   users.users.lldap = {
     name = "lldap";

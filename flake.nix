c8c068ef6d089fd863a7db5b71d179fb68878b80
Alexander Ratajczak <a4blue@hotmail.de>
2026-03-17 07:31:03 +0100

    update

--- flake.nix
+++ flake.nix
@@ -46,7 +46,6 @@
 
     nix-vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions/master";
-      #url = "github:nix-community/nix-vscode-extensions/909232203bc0bc4c1e3311f41be190322bb04a94";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 

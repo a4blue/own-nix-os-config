0ba91f4309c7b8d97bfd29ccec29f67d5d92b518
Alexander Ratajczak <a4blue@hotmail.de>
2026-05-01 21:45:32 +0200

    new blocky config

--- systems/homelab/configs/blocky.nix
+++ systems/homelab/configs/blocky.nix
@@ -15,27 +15,28 @@
       keyFile = "/nix/secret/blocky_cert/blocky.key";
       log.level = "warn";
       upstreams.groups.default = [
-        # Owners:
-        #"tcp-tls:ns26.de.dns.opennic.glue" # probst.click, cert is expired
-        "tcp-tls:ns1.fr.dns.opennic.glue#dns.elanceleur.org" # EricL
-        "tcp-tls:ns1.no.dns.opennic.glue#ns4.opennameserver.org" # opennameserver.org
+        "https://opennic.i2pd.xyz/dns-query" # ns8.fr
         "tcp-tls:ns5.fi.dns.opennic.glue#dns.froth.zone" # sammefishe
-        "tcp-tls:ns3.de.dns.opennic.glue#dns.geekbyte.de" # Koby
-        "tcp-tls:ns13.de.dns.opennic.glue#ns1.opennameserver.org" # opennameserver.org
-        "tcp-tls:ns16.de.dns.opennic.glue#ns2.opennameserver.org" # opennameserver.org
-        "tcp-tls:ns18.de.dns.opennic.glue#ns3.opennameserver.org" # opennameserver.org
-        #"tcp-tls:ns23.de.dns.opennic.glue#dns3.opennic.probst.click" # probst.click, cert expired
-        "tcp-tls:ns28.de.dns.opennic.glue#jabber-germany.de" # mistersixt
-        "tcp-tls:ns29.de.dns.opennic.glue#morbitzer.de" # mistersixt
+        "https://korridor.semia-infra.net/dns-query" # ns2.at
+        "tcp-tls:ns2.de.dns.opennic.glue:65533#kekew.info"
+        "tcp-tls:ns3.de.dns.opennic.glue#dns.furrytech.io"
+        "tcp-tls:ns29.de.dns.opennic.glue#morbitzer.de"
+        "https://opennic2.eth-services.de:853" # ns31.de
+        "https://ns1.opennameserver.org/dns-query"
+        "https://ns4.opennameserver.org/dns-query"
       ];
       bootstrapDns = [
         {
-          upstream = "tcp-tls:ns2.de.dns.opennic.glue:853#kekew.info";
-          ips = ["80.152.203.134" "2003:a:64b:3b00::2"];
+          upstream = "tcp-tls:ns4.de.dns.opennic.glue:65533#kekew.info";
+          ips = ["80.153.146.105" "2003:a:812:5700::6"];
         }
         {
-          upstream = "tcp-tls:ns28.de.dns.opennic.glue:853#jabber-germany.de";
-          ips = ["152.53.15.127" "2a03:4000:006b:0191:9825:1cff:fe34:0bbe"];
+          upstream = "tcp-tls:ns8.fr.dns.opennic.glue#opennic.i2pd.xyz";
+          ips = ["91.190.185.43" "2001:470:1f15:b80::53"];
+        }
+        {
+          upstream = "tcp-tls:ns4.pl.dns.opennic.glue#opennic2.i2pd.xyz";
+          ips = ["185.226.181.19" "2001:470:71:6dc::53"];
         }
       ];
       customDNS = {

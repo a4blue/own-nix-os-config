d5f9d7a8c2410e409b8df3401ce20483038980de
Alexander Ratajczak <a4blue@hotmail.de>
2026-09-12 18:39:26 +0200

    update

--- systems/homelab/configs/ollama.nix
+++ systems/homelab/configs/ollama.nix
@@ -10,7 +10,7 @@
     ollama = {
       enable = true;
       package = pkgs.ollama-vulkan;
-      loadModels = ["qwen3.5:9b" "qwen3.5:27b"];
+      loadModels = ["qwen3.5:9b" "qwen3.5:27b" "qwen3.8:27b"];
       syncModels = true;
     };
   };

641b97f44106ba9c14a87d0d1694dc386c9ba4f5
Alexander Ratajczak <a4blue@hotmail.de>
2026-09-16 08:45:11 +0200

    update

--- overlays/previous.nix
+++ overlays/previous.nix
@@ -6,6 +6,8 @@
 }: {
   nixpkgs.overlays = [
     (final: prev: {
+      # TODO remove after nix-SOPS Update
+      buildGo125Module = prev.buildGoModule;
       #python313 = prev.python313.override {
       #  packageOverrides = pyfinal: pyprev: {
       #    pyrate-limiter = pyprev.pyrate-limiter.overridePythonAttrs (old: {
